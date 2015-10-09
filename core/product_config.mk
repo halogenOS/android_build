b3638dc07fb7c02ec4d085cc00d3ff658ef13374
Alex Cruz <mazdarider23@gmail.com>
2015-10-09 02:56:37 +0000

    build: fix listing of configs
    Cherry pick of "Bring a few things inline with our lollipop branch"
    
    Change-Id: I8ece9573d6f5a5d801b26e1e71abc501ac541f60
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(XOS_BUILD),)
-    all_product_configs := $(shell ls device/*/$(XOS_BUILD)/cm.mk)
+    all_product_configs := $(shell ls device/*/$(XOS_BUILD)/XOS.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
@@ -267,7 +267,6 @@ endif
 current_product_makefile :=
 all_product_makefiles :=
 all_product_configs :=
-
 
 #############################################################################
 
