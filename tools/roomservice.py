814347dfd0473bfdcdecb85420b87485be12101f
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-08 11:32:30 +0200

    Revert "roomservice: allow replacing already existing projects using deps"
    We don't really want anyone to overwrite existing repositories.
    If they really need it, they can do it locally. We don't want other
    devices to be affected by a replaced repo on multi-device build machines.
    
    This reverts commit 68250bc95f3f0ad6f7f11f996a795d7eafea6b27.
    
    Change-Id: If44754a97a47deda2539df61bd1cf95aa0b39a32

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -72,11 +72,8 @@ if not os.path.exists(local_manifests): os.makedirs(local_manifests)
 
 def exists_in_tree(lm, repository):
     for child in lm.getchildren():
-        try:
-            if child.attrib['path'].endswith(repository):
-                return child
-        except:
-            pass
+        if child.attrib['path'].endswith(repository):
+            return child
     return None
 
 def exists_in_tree_device(lm, repository):
@@ -145,12 +142,6 @@ def add_to_manifest_dependencies(repositories):
     except:
         lm = ElementTree.Element("manifest")
 
-    try:
-        mlm = ElementTree.parse(".repo/manifests/default.xml")
-        mlm = mlm.getroot()
-    except Exception as e:
-        mlm = ElementTree.Element("manifest")
-
     for repository in repositories:
         repo_name = repository['repository']
         repo_target = repository['target_path']
@@ -180,13 +171,6 @@ def add_to_manifest_dependencies(repositories):
                 print 'updating branch for %s to %s' % (repo_name, repository['branch'])
                 existing_project.set('revision', repository['branch'])
             continue
-        existing_m_project = exists_in_tree(mlm, repo_target)
-        if existing_m_project != None:
-            if existing_m_project.attrib['path'] == repository['target_path']:
-                print '%s already exists in main manifest, replacing with new dep' % (repo_name)
-                lm.append(ElementTree.Element("remove-project", attrib = {
-                    "name": existing_m_project.attrib['name']
-                }))
 
         print 'Adding dependency: %s -> %s' % (repo_name, repo_target)
         project = ElementTree.Element("project", attrib = { "path": repo_target,
